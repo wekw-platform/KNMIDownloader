006fc830ae215d0d04002434aae8fc108303886c
rvh033 <ricovhaaren.rv@gmail.com>
2025-08-04 01:04:55 +0200

    final++

--- src/knmidownloader/Logger.cs
+++ src/knmidownloader/Logger.cs
@@ -3,10 +3,8 @@
     internal class Logger
     {
 
-        public int Calls;
         public void Print(string source, string msg)
         {
-            ++Calls;
             Console.WriteLine($"[{source}] [{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] {msg}");
         }
     }
